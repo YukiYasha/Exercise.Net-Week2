f1465b41fdf26909e80b95818008038a044f03e0
Yuki Yasha <thuongbtvt@gmail.com>
2018-10-10 19:17:50 +0700

    Finished 90%
    Load danh sách ngày vào cboNgay phù hợp theo điều kiện tháng và năm

---
+++ Ex.Net-W2/Ex01/Bai01.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Ex01
+{
+    partial class Bai01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.cboNgay = new System.Windows.Forms.ComboBox();
+            this.cboThang = new System.Windows.Forms.ComboBox();
+            this.cboNam = new System.Windows.Forms.ComboBox();
+            this.btnTinhThu = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(54, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÍNH THỨ TRONG TUẦN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ngày";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tháng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Năm";
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKQ.Location = new System.Drawing.Point(60, 167);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(121, 16);
+            this.lblKQ.TabIndex = 4;
+            this.lblKQ.Text = "Hôm đó là ngày ";
+            this.lblKQ.Visible = false;
+            // 
+            // cboNgay
+            // 
+            this.cboNgay.DropDownHeight = 68;
+            this.cboNgay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboNgay.FormattingEnabled = true;
+            this.cboNgay.IntegralHeight = false;
+            this.cboNgay.Location = new System.Drawing.Point(79, 51);
+            this.cboNgay.Name = "cboNgay";
+            this.cboNgay.Size = new System.Drawing.Size(121, 21);
+            this.cboNgay.TabIndex = 5;
+            // 
+            // cboThang
+            // 
+            this.cboThang.DropDownHeight = 68;
+            this.cboThang.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboThang.FormattingEnabled = true;
+            this.cboThang.IntegralHeight = false;
+            this.cboThang.Location = new System.Drawing.Point(79, 87);
+            this.cboThang.Name = "cboThang";
+            this.cboThang.Size = new System.Drawing.Size(121, 21);
+            this.cboThang.TabIndex = 6;
+            this.cboThang.SelectedValueChanged += new System.EventHandler(this.cboThang_SelectedValueChanged);
+            // 
+            // cboNam
+            // 
+            this.cboNam.DropDownHeight = 68;
+            this.cboNam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboNam.FormattingEnabled = true;
+            this.cboNam.IntegralHeight = false;
+            this.cboNam.Location = new System.Drawing.Point(79, 123);
+            this.cboNam.Name = "cboNam";
+            this.cboNam.Size = new System.Drawing.Size(121, 21);
+            this.cboNam.TabIndex = 7;
+            this.cboNam.SelectedValueChanged += new System.EventHandler(this.cboNam_SelectedValueChanged);
+            // 
+            // btnTinhThu
+            // 
+            this.btnTinhThu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinhThu.Location = new System.Drawing.Point(223, 51);
+            this.btnTinhThu.Name = "btnTinhThu";
+            this.btnTinhThu.Size = new System.Drawing.Size(75, 23);
+            this.btnTinhThu.TabIndex = 8;
+            this.btnTinhThu.Text = "Tính Thứ";
+            this.btnTinhThu.UseVisualStyleBackColor = true;
+            this.btnTinhThu.Click += new System.EventHandler(this.btnTinhThu_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLamLai.Location = new System.Drawing.Point(223, 87);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(75, 23);
+            this.btnLamLai.TabIndex = 9;
+            this.btnLamLai.Text = "Làm lại";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(223, 123);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Bai01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(328, 209);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnTinhThu);
+            this.Controls.Add(this.cboNam);
+            this.Controls.Add(this.cboThang);
+            this.Controls.Add(this.cboNgay);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Bai01";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tính thứ trong tuần";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblKQ;
+        private System.Windows.Forms.ComboBox cboNgay;
+        private System.Windows.Forms.ComboBox cboThang;
+        private System.Windows.Forms.ComboBox cboNam;
+        private System.Windows.Forms.Button btnTinhThu;
+        private System.Windows.Forms.Button btnLamLai;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ Ex.Net-W2/Ex01/Bai01.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex01
+{
+    public partial class Bai01 : Form
+    {
+        int Day = 0, Month = 0, Year = 0;
+        public Bai01()
+        {            
+            InitializeComponent();
+            LoadCb();            
+        }
+
+        private void LoadCb()
+        {
+            //Load Ngày Tháng Năm vào cb tương ứng
+            
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {            
+            this.Dispose();
+        }
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            btnTinhThu.Enabled = true;
+            lblKQ.Visible = false;
+            lblKQ.Text = "Hôm đó là ngày ";
+        }
+
+        private void cboNam_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cboNam.Text == "")
+                return;
+            Year = int.Parse(cboNam.Text);
+
+            if (Month == 2)
+            {
+                if (((Year % 4) == 0 && (Year % 100) == 0 && (Year % 400) == 0) || ((Year % 4) == 0 && (Year % 100) != 0))
+                {
+                    //danh sách ngày từ 1-29
+                }
+                else
+                {
+                    //danh sách ngày từ 1-28
+                }
+            }
+        }
+
+        private void cboThang_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cboThang.Text == "")
+                return;
+            Month = int.Parse(cboThang.Text);
+
+            if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+            {
+                //danh sách ngày từ 1-31
+            }
+            else
+            if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
+            {
+                //danh sách ngày từ 1-30
+            }
+            else
+                cboNam_SelectedValueChanged(sender, e);
+        }
+
+        private void btnTinhThu_Click(object sender, EventArgs e)
+        {
+            if(cboNam.Text == "" || cboThang.Text == "" || cboNgay.Text == "")
+            {
+                MessageBox.Show("Chọn đầy đủ Ngày Tháng Năm!!!", "Cảnh báo", MessageBoxButtons.RetryCancel);
+            }
+            else
+            {
+                DateTime date = new DateTime(Year, Month, Day);
+                lblKQ.Text += TranslateDay((date.DayOfWeek).ToString());
+                lblKQ.Visible = true;
+                btnTinhThu.Enabled = false;
+            }            
+        }
+
+        private string TranslateDay(string x)
+        {
+            switch(x)
+            {
+                case "Monday":
+                    return "Thứ Hai";
+                case "Tueday":
+                    return "Thứ Ba";
+                case "Wednesday":
+                    return "Thứ Tư";
+                case "Thursday":
+                    return "Thứ Năm";
+                case "Friday":
+                    return "Thứ Sáu";
+                case "Saturday":
+                    return "Thứ Bảy";
+                case "Sunday":
+                    return "Chủ Nhật";
+                default:
+                    return "Không có ngày";
+            }
+        }
+    }
+}
